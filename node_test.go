c0650e413a6061ea05c332f4676b97f3a6bd64dd
Mark Rushakoff <mark.rushakoff@gmail.com>
2025-02-10 16:59:44 -0500

    test: assert unknown certificates reject dial
    In both directions.

--- node_test.go
+++ node_test.go
@@ -55,7 +55,9 @@ func TestNewNode(t *testing.T) {
 	defer cancel()
 }
 
-func TestNode_Dial(t *testing.T) {
+// Intra-network dial should just work.
+func TestNode_Dial_ok(t *testing.T) {
+	t.Parallel()
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -67,4 +69,51 @@ func TestNode_Dial(t *testing.T) {
 	require.NoError(t, err)
 
 	defer conn.Close(1, "test stopping")
+}
+
+func TestNode_Dial_unrecognizedCert(t *testing.T) {
+	t.Run("neither client nor server know each other", func(t *testing.T) {
+		t.Parallel()
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		nw := dragontest.NewNetwork(t, ctx, dcatest.FastConfig(), dcatest.FastConfig())
+		defer nw.Wait()
+		defer cancel()
+
+		out := dragontest.NewNetwork(t, ctx, dcatest.FastConfig())
+		defer out.Wait()
+		defer cancel()
+
+		conn, err := out.Nodes[0].Node.DialPeer(ctx, nw.Nodes[0].UDP.LocalAddr())
+		require.Error(t, err)
+		require.Nil(t, conn)
+	})
+
+	t.Run("one-way knowledge", func(t *testing.T) {
+		t.Parallel()
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		cfg1, cfg2 := dcatest.FastConfig(), dcatest.FastConfig()
+		nw12 := dragontest.NewNetwork(t, ctx, cfg1, cfg2)
+		defer nw12.Wait()
+		defer cancel()
+
+		nw123 := dragontest.NewNetwork(t, ctx, cfg1, cfg2, dcatest.FastConfig())
+		defer nw123.Wait()
+		defer cancel()
+
+		t.Run("client knows server, but server does not know client", func(t *testing.T) {
+			conn, err := nw123.Nodes[2].Node.DialPeer(ctx, nw12.Nodes[1].UDP.LocalAddr())
+			require.Error(t, err)
+			require.Nil(t, conn)
+		})
+
+		t.Run("server knows client, but client does not know server", func(t *testing.T) {
+			conn, err := nw12.Nodes[1].Node.DialPeer(ctx, nw123.Nodes[2].UDP.LocalAddr())
+			require.Error(t, err)
+			require.Nil(t, conn)
+		})
+	})
 }
