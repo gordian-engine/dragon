e2ee8252150a83a7c17d75e1565b36e5bf797fa4
Mark Rushakoff <mark.rushakoff@gmail.com>
2025-06-09 13:45:20 -0400

    test: add view manager connectivity test
    I considered making this a little more modular to allow injecting any
    view manager implementation, but we aren't at the point where we need
    that yet.
    
    The current included test case passes, but there are some unincluded
    cases that result in unclear failures. The failure reports all the
    connectivity sets, but the unclear part is that there is overlap between
    the sets. So a failing 4-node case might be [0 1 2] and [0 1 3] -- which
    overlaps and doesn't make sense -- as opposed to a disjoint collection
    of [0 1] and [2 3].
    
    I need to debug further to understand where the root cause is, but this
    was a good commit checkpoint.

---
+++ dview/dviewtest/doc.go
@@ -0,0 +1,2 @@
+// Package dviewtest contains test helpers for working with the [dview] package.
+package dviewtest
---
+++ internal/integration/viewmanager_test.go
@@ -0,0 +1,261 @@
+package integration
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"net"
+	"sort"
+	"testing"
+	"time"
+
+	"github.com/bits-and-blooms/bitset"
+	"github.com/gordian-engine/dragon"
+	"github.com/gordian-engine/dragon/dcert/dcerttest"
+	"github.com/gordian-engine/dragon/dconn"
+	"github.com/gordian-engine/dragon/dragontest"
+	"github.com/gordian-engine/dragon/dview/dviewrand"
+	"github.com/gordian-engine/dragon/internal/dtest"
+	"github.com/stretchr/testify/require"
+)
+
+func TestRandViewManager(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping due to short mode")
+	}
+
+	for _, c := range []struct {
+		nNodes int
+
+		active, passive int
+	}{
+		// This is just an arbitrary selection that seems to pass consistently.
+		// TODO: this needs more cases, but there are some combinations
+		// that seem to tickle edge cases where the network graph isn't fully connected.
+		{nNodes: 5, active: 4, passive: 6},
+	} {
+		name := fmt.Sprintf(
+			"nNodes=%d active=%d passive=%d",
+			c.nNodes, c.active, c.passive,
+		)
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			testRandViewManager(
+				t,
+				c.nNodes,
+				c.active, c.passive,
+			)
+		})
+	}
+}
+
+func testRandViewManager(
+	t *testing.T,
+	nNodes, activeViewSize, passiveViewSize int,
+) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	log := dtest.NewLogger(t)
+
+	caConfigs := make([]dcerttest.CAConfig, nNodes)
+	for i := range nNodes {
+		caConfigs[i] = dcerttest.FastConfig()
+	}
+	dNet := dragontest.NewNetwork(
+		t, ctx,
+		caConfigs,
+		func(idx int, nc dragontest.NodeConfig) dragon.NodeConfig {
+			out := nc.ToDragonNodeConfig()
+
+			// The view manager created through NewDefaultNetwork
+			// matches the view size to the node count,
+			// and we specifically want a smaller view size for this test.
+			out.ViewManager = dviewrand.New(
+				log.With("node_sys", "view_manager"),
+				dviewrand.Config{
+					ActiveViewSize:  activeViewSize,
+					PassiveViewSize: passiveViewSize,
+
+					// Unique seed for each node.
+					RNG: rand.New(rand.NewPCG(uint64(idx), uint64(activeViewSize*passiveViewSize))),
+				},
+			)
+			return out
+		},
+	)
+
+	// First build up all the node IDs,
+	// because each connection observer needs this mapping.
+	nodeIDsBySPKI := make(map[string]int, nNodes)
+	connObservers := make([]*connObserver, nNodes)
+	for i := range nNodes {
+		nodeIDsBySPKI[string(dNet.Chains[i].Leaf.RawSubjectPublicKeyInfo)] = i
+		connObservers[i] = newConnObserver(t, nodeIDsBySPKI)
+	}
+
+	for i, o := range connObservers {
+		go o.run(ctx, dNet.ConnectionChanges[i])
+	}
+	defer cancel()
+
+	// Now dial everything.
+	// Each node alternates connecting to node 0 or 1,
+	// so they don't all dial the same peer,
+	// but they dial a small set of nodes which should propagate correctly.
+	for i := range nNodes {
+		var target net.Addr
+		if i&1 == 1 {
+			target = dNet.Nodes[0].UDP.LocalAddr()
+		} else {
+			target = dNet.Nodes[1].UDP.LocalAddr()
+		}
+
+		err := dNet.Nodes[i].Node.DialAndJoin(ctx, target)
+		if i == 1 {
+			// Node 1 will dial Node 0,
+			// but Node 0 should have already dialed Node 1.
+			// We almost always get the already-connected-to-certificate error,
+			// but once in a while we get the address error.
+			// Either one is acceptable in this case.
+			require.Error(t, err)
+			var certErr dragon.AlreadyConnectedToCertError
+			var addrErr dragon.AlreadyConnectedToAddrError
+			switch {
+			case errors.As(err, &certErr):
+				require.True(t, certErr.Chain.Leaf.Equal(dNet.Chains[0].Leaf))
+			case errors.As(err, &addrErr):
+				require.Equal(t, target.String(), addrErr.Addr)
+			default:
+				t.Fatalf("got unexpected error type %T (%v)", err, err)
+			}
+		} else {
+			require.NoErrorf(
+				t, err,
+				"failed to dial from node %d", i,
+			)
+		}
+	}
+
+	// Short sleep to give network time to stabilize if necessary.
+	// It might be better to actually add another way
+	// to see some elapsed time without any connection changes,
+	// but for now this is good enough.
+	time.Sleep(200 * time.Millisecond)
+
+	// Now we track all the connected nodes using "union find":
+	// https://en.wikipedia.org/wiki/Disjoint-set_data_structure
+	groups := make([]*bitset.BitSet, nNodes)
+	for i, o := range connObservers {
+		bs := bitset.MustNew(uint(nNodes))
+		groups[i] = bs
+
+		cn := o.ConnectedNodes()
+
+		// Make the bitset for the current node.
+		for _, ci := range cn {
+			bs.Set(uint(ci))
+		}
+
+		// Now, this newest bitset takes precedence
+		// over any previously created bitset.
+		for _, ci := range cn {
+			if groups[ci] == nil {
+				continue
+			}
+			bs.InPlaceUnion(groups[ci])
+			groups[ci] = bs
+		}
+	}
+
+	// Finally, if the graph is fully connected,
+	// the last bitset should have all the bits set.
+	finalGroup := groups[len(groups)-1]
+	if finalGroup.Count() != uint(nNodes) {
+		t.Error("FAIL: graph was not fully connected")
+
+		seen := map[*bitset.BitSet]struct{}{}
+		for _, g := range groups {
+			if _, ok := seen[g]; ok {
+				continue
+			}
+			seen[g] = struct{}{}
+
+			t.Error(g.String())
+		}
+		t.FailNow()
+	}
+	require.Equalf(
+		t, uint(nNodes), finalGroup.Count(),
+		"final bitset was incomplete: %s", finalGroup.String(),
+	)
+}
+
+// connObserver is a helper for TestRandViewManager
+// that allows interrogating the node IDs
+// connected to a particular node.
+type connObserver struct {
+	Done chan struct{}
+
+	nodeIDsBySPKI map[string]int
+
+	nodeRequests chan chan []int
+}
+
+func newConnObserver(t *testing.T, nodeIDsBySPKI map[string]int) *connObserver {
+	o := &connObserver{
+		Done: make(chan struct{}),
+
+		nodeIDsBySPKI: nodeIDsBySPKI,
+
+		nodeRequests: make(chan chan []int),
+	}
+	t.Cleanup(func() {
+		<-o.Done
+	})
+	return o
+}
+
+func (o *connObserver) run(ctx context.Context, connChanges <-chan dconn.Change) {
+	defer close(o.Done)
+
+	connectedNodeIDs := map[int]struct{}{}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case cc := <-connChanges:
+			nodeID := o.nodeIDsBySPKI[string(cc.Conn.Chain.Leaf.RawSubjectPublicKeyInfo)]
+			if cc.Adding {
+				connectedNodeIDs[nodeID] = struct{}{}
+			} else {
+				delete(connectedNodeIDs, nodeID)
+			}
+
+		case ch := <-o.nodeRequests:
+			out := make([]int, 0, len(connectedNodeIDs))
+			for i := range connectedNodeIDs {
+				out = append(out, i)
+			}
+
+			ch <- out
+		}
+	}
+}
+
+// ConnectedNodes returns a sorted slice of the node indices
+// that the observer's node is connected to.
+func (o *connObserver) ConnectedNodes() []int {
+	nodesCh := make(chan []int, 1)
+
+	// Not bothering with context checking in this particular test.
+	o.nodeRequests <- nodesCh
+
+	got := <-nodesCh
+	sort.Ints(got)
+	return got
+}
